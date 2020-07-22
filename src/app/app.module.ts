78b22eb463ef0338fb4cca5943f697cef48a3992
nico1512 <nicoassanelli@hotmail.com>
2020-07-22 18:58:57 -0300

    Componentes
    Se crean componentes para examen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CrearClienteComponent } from './crear-cliente/crear-cliente.component';
+import { CrearRutinaComponent } from './crear-rutina/crear-rutina.component';
+import { TablaClienteComponent } from './tabla-cliente/tabla-cliente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrearClienteComponent,
+    CrearRutinaComponent,
+    TablaClienteComponent
   ],
   imports: [
     BrowserModule,
